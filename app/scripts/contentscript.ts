a3085065db968818465691a7440422f5a2338c2e
Arohan Dutt <arohandutt@gmail.com>
2020-12-19 19:13:22 -0500

    Add Nose

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -2,3 +2,14 @@
 import 'chromereload/devonly'
 
 console.log(`'Allo 'Allo! Content script`);
+
+const appPointer = document.createElement('div')
+appPointer.id = "appPointer"
+appPointer.innerText = "👃🏾"
+appPointer.setAttribute("style", `top:20%; left:20%`)
+document.body.appendChild(appPointer)
+
+// chrome.tabs.create({
+//     index: 0,
+//     pinned: true
+// })
